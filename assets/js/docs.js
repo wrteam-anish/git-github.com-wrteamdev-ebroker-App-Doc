46caf30dfdba602341f28f9a94c086a09597ef97
wrteam-anish <wrteam.anish@gmail.com>
2023-03-17 14:42:17 +0530

    eBroker Documentation

---
+++ assets/js/docs.js
@@ -0,0 +1,122 @@
+"use strict";
+
+/* ====== Define JS Constants ====== */
+const sidebarToggler = document.getElementById('docs-sidebar-toggler');
+const sidebar = document.getElementById('docs-sidebar');
+const sidebarLinks = document.querySelectorAll('#docs-sidebar .scrollto');
+
+
+
+/* ===== Responsive Sidebar ====== */
+function insertAfter(referenceNode, newNode) {
+referenceNode.parentNode.insertBefore(newNode,
+	 referenceNode.nextSibling,
+	);
+}
+  
+window.onload=function() 
+{ 
+
+
+    responsiveSidebar(); 
+
+	var images=document.getElementsByTagName("img");
+for(var i=0;i<images.length;i++){
+
+	if(images[i].dataset.noapply=="true"){
+
+	}else{
+		images[i].width=window.innerWidth/2;
+
+	}
+
+}
+};
+
+window.onresize=function() 
+{ 
+    responsiveSidebar(); 
+};
+
+
+function responsiveSidebar() {
+    let w = window.innerWidth;
+	if(w >= 1200) {
+	    // if larger 
+	    console.log('larger');
+		sidebar.classList.remove('sidebar-hidden');
+		sidebar.classList.add('sidebar-visible');
+		
+	} else {
+	    // if smaller
+	    console.log('smaller');
+	    sidebar.classList.remove('sidebar-visible');
+		sidebar.classList.add('sidebar-hidden');
+	}
+};
+
+sidebarToggler.addEventListener('click', () => {
+	if (sidebar.classList.contains('sidebar-visible')) {
+		console.log('visible');
+		sidebar.classList.remove('sidebar-visible');
+		sidebar.classList.add('sidebar-hidden');
+		
+	} else {
+		console.log('hidden');
+		sidebar.classList.remove('sidebar-hidden');
+		sidebar.classList.add('sidebar-visible');
+	}
+});
+
+
+/* ===== Smooth scrolling ====== */
+/*  Note: You need to include smoothscroll.min.js (smooth scroll behavior polyfill) on the page to cover some browsers */
+/* Ref: https://github.com/iamdustan/smoothscroll */
+
+sidebarLinks.forEach((sidebarLink) => {
+	
+	sidebarLink.addEventListener('click', (e) => {
+		
+		e.preventDefault();
+		
+		var target = sidebarLink.getAttribute("href").replace('#', '');
+		
+		//console.log(target);
+		
+        document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
+        
+        
+        //Collapse sidebar after clicking
+		// if (sidebar.classList.contains('sidebar-visible') && window.innerWidth < 1200){
+			
+		// 	sidebar.classList.remove('sidebar-visible');
+		//     sidebar.classList.add('sidebar-hidden');
+		// } 
+		
+    });
+	
+});
+
+
+/* ===== Gumshoe SrollSpy ===== */
+/* Ref: https://github.com/cferdinandi/gumshoe  */
+// Initialize Gumshoe
+var spy = new Gumshoe('#docs-nav a', {
+	offset: 69 //sticky header height
+});
+
+
+/* ====== SimpleLightbox Plugin ======= */
+/*  Ref: https://github.com/andreknieriem/simplelightbox */
+// var lightbox = new SimpleLightbox('img', {/* options */});
+
+
+
+
+
+
+
+
+
+
+
